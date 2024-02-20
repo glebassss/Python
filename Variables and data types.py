317ea0e920d674bd6d91329d4a6f6552843cdcd9
Gleb Kleshchenko <116480129+glebassss@users.noreply.github.com>
2024-02-20 20:23:21 +0300

    1.Create Variables and datatypes.py 2.Was write examples for data types

---
+++ Variables and data types.py
@@ -0,0 +1,29 @@
+#This file shows 2 exaples of 5 data types
+#1. Boolean
+print("1. Boolean data type")
+boolean1=True
+print(boolean1, type(boolean1))
+boolean2=False
+print(boolean2, type(boolean2))
+#2. String
+print("2. String data type")
+string1="Hello World"
+print(string1,type(string1))
+string2="Hello Python"
+print(string2,type(string2))
+#3. Integer
+print("3. String data type")
+integer1=10
+print(integer1,type(integer1))
+integer2=20
+print(integer2,type(integer2))
+#4. Float
+print("4. Float data type")
+float1=1.2
+print(float1,type(float1))
+float2=33.1
+print(float2,type(float2))
+#5. NoneType
+print("5. None data type")
+none1=None
+print(none1,type(none1))
