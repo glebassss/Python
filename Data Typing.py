cc1397cf1c6ebdb54b94f4f7328d33059e269e57
Gleb Kleshchenko <116480129+glebassss@users.noreply.github.com>
2024-02-21 20:48:03 +0300

    1.Create Data Typing.py 2.Was change data type from string to integer

---
+++ Data Typing.py
@@ -0,0 +1,4 @@
+# Change the data type of the str=”123” variable from string to numeric. Show the result.
+str="123"
+str=int(str)   #Change data type from string to integer
+print(type(str)) #Show the result
