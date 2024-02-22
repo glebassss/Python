61d5db9b9b8ecae5e7dd3b161bd1cc963bf48705
Gleb Kleshchenko <116480129+glebassss@users.noreply.github.com>
2024-02-22 20:19:18 +0300

    1.Create Numbers.py 2.Show some examples about operations with numbers.

---
+++ Numbers.py
@@ -0,0 +1,27 @@
+#Importing library "Math"
+import math
+#Exponentiation
+print(1**100)
+#or
+print(math.pow(1, 100))
+
+print(2**16)
+#or
+print(math.pow(2, 16))
+#Subtraction
+print(2-1000)
+#Multiplication and summation
+print(2+2*2)
+#Division
+print(60/3)
+#Square root of a number
+math.sqrt(144)
+#Finding the unknown
+x=2+5*14
+print(x)
+
+y=60-11
+print(y)
+
+z=pow(16-8, 2)
+print(z)
