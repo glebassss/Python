edfc1451f7437aee012a9d4d20209448ac21c731
Gleb Kleshchenko <116480129+glebassss@users.noreply.github.com>
2024-02-22 21:36:03 +0300

    1.Create Strings.py 2.Show some examples about operations with strings.

---
+++ Strings.py
@@ -0,0 +1,22 @@
+#Ways to notate strings
+str1= "Hello World"
+print(str1)
+str2= 'Hello World'
+print(str2)
+#Applying addition and multiplication operations to strings
+print('-'*100)
+#Contactenation
+hello='Hello'
+world='world'
+print(hello+' '+world)
+#Data Types can convert to string
+num=1   #Number
+print(str(num), type(str(num)))
+fl=1.5      #Float
+print(str(fl), type(str(fl)))
+Bolean= True    #Boolean
+print(str(Bolean), type(str(Bolean)))
+none=None #None
+print(str(none), type(str(none)))
+
+
