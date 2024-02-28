b3db9f9c31152337d25f22c51731646341473d7d
Gleb Kleshchenko <116480129+glebassss@users.noreply.github.com>
2024-02-28 11:34:33 +0300

    1.Create Practice 1. Numbers and exception handlers.py 2.Solved task on knowledge of exceptions

--- Exception handlers.py
+++ Exception handlers.py
@@ -12,7 +12,7 @@ except TypeError:
     except ValueError:
         print('Wrong value in "numberTwo", change it to integer')
 finally:
-    print('Task 1 complete')
+    print('Task 1 complete\n')
 
 # Task 2. The year, month number and day of birth of a person are known,
 # as well as the day, year and month number of today. Determine the person's age (number of completed years).
@@ -27,4 +27,22 @@ personDay = currentDay - birthDay
 print(personDay)
 personYear = personDay.days//365
 print('The person is ' + str(personYear) + ' y.o.')
-print('Task 2 complete')
+print('Task 2 complete\n')
+
+# Task 3. The year, month number and birthday of each of the two people are known.
+# Determine which of them is older.
+FirstYear = input('First person. Enter a year : ')
+FirstMonth = input('First person. Enter a month: ')
+FirstDay = input('First person. Enter a day: ')
+FirstBirthday = date(int(FirstYear), int(FirstMonth), int(FirstDay))
+print(FirstBirthday)
+SecondYear = input('Second person. Enter a year : ')
+SecondMonth = input('Second person. Enter a month: ')
+SecondDay = input('Second person. Enter a day: ')
+SecondBirthday = date(int(SecondYear), int(SecondMonth), int(SecondDay))
+print(SecondBirthday)
+Differense= FirstBirthday-SecondBirthday
+if Differense.days > 0:
+    print('The Second person is older than the First person')
+else:
+    print('The First Person is older than the Second person')
