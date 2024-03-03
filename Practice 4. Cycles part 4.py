0e54523a8f61a11696661b54839d4931d598cc64
Gleb Kleshchenko <116480129+glebassss@users.noreply.github.com>
2024-03-03 15:13:37 +0300

    1. Add 'height +1' in third line

--- Practice 4. Cycles part 4.py
+++ Practice 4. Cycles part 4.py
@@ -1,5 +1,5 @@
 # Task. The user enters the height of the triangle. Construct a right triangle
 height = int(input("Enter a height of triangle: "))
-for i in range(height):
-    print("*"*i,end="\n")
+for i in range(height+1):
+    print("*"*i, end="\n")
 print('Task complete')
